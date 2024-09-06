30787e5701a02f87a2c35bb4136275b21c9c6ff4
𝑫𝑬𝑪𝑶𝑫𝑬𝑹44 <gplas234@gmail.com>
2024-09-06 19:08:45 +0200

    Add files via upload

---
+++ Main.sh
@@ -0,0 +1,31 @@
+clear
+figlet -f mono9 "Decoder 44" | lolcat
+cyan='[0;36m'
+lightgreen='[1;32m'
+red='[1;31m'
+yellow='[1;33m'
+NC="\033[0m"
+echo -e $cyan"[1m : Developed By "$yellow"https://t.me/decoder44"
+echo " "
+
+echo -e "[1m[33m
+$ What is Your [31mBanner [33mName[32m :
+
+${NC}"
+read varbanner
+echo
+echo -e "[1m[33m
+$ What is Your Cowsay Name[32m :
+
+ ${NC}"
+read varcowsay
+echo  "cowsay -f eyes "$varcowsay" | lolcat" > /data/data/com.termux/files/usr/themes/temp
+echo "toilet -f big ' $varbanner' -F gay | lolcat" > /data/data/com.termux/files/usr/themes/Main
+echo
+echo "clear" > /data/data/com.termux/files/usr/themes/remove
+echo "  "
+echo "  "
+cat "/data/data/com.termux/files/usr/themes/remove" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+cat "/data/data/com.termux/files/usr/themes/temp" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+
+cat "/data/data/com.termux/files/usr/themes/Main" >> /data/data/com.termux/files/usr/etc/bash.bashrc
---
+++ Requirements.sh
@@ -0,0 +1,18 @@
+NC="\033[0m"
+clear
+apt update
+apt upgrade
+apt install figlet -y
+figlet Decoder44
+apt install toilet -y
+apt install cowsay -y
+apt install nano -y
+apt install ruby -y
+gem install lolcat
+mkdir /data/data/com.termux/files/usr/themes
+clear
+figlet -f mono9 Done. | lolcat
+echo
+echo -e "[1m Now Run bash Main.sh${NC}"
+echo
+echo -e "[1m[32m Developed By :[33m https://t.me/decoder44${NC}"
